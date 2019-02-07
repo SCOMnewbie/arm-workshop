5ee24c0d61fd91dc65dd6d3ad973f12f023f1516
Francois LEON <francois.leon@ubisoft.com>
2019-02-07 21:54:24 +0100

    update

--- lab1/Deploy-Template.ps1
+++ lab1/Deploy-Template.ps1
@@ -1,3 +1,6 @@
+<#
+OK, so our template is now looking pretty good. The major issue with it is that it does not ensure that the storageAccount will be unique and therefore the success of the deployment cannot be assured. Let’s fix that by using a variable and some of the functions that are available.
+#>
 $Rg = "lab1"
 $location = "West Europe"
 $job = "job2"
@@ -6,7 +9,7 @@ $template = '.\azuredeploy.json'
 
 az group create -g $rg -l $location
 #Won't work with the message New-AzureRmResourceGroupDeployment : 21:28:31 - Error: Code=InvalidTemplate; Message=Deployment template validation failed:..
-New-AzureRmRe
+#New-AzureRmResourceGroupDeployment -Name $job -storageAccount $storageAccount -accounttype "Premium_RAGRS" -TemplateFile $template -ResourceGroupName $rg
 #Will work
 New-AzureRmResourceGroupDeployment -Name $job -storageAccount $storageAccount -TemplateFile $template -ResourceGroupName $rg
 
--- lab1/OLD/V2-Deploy-Template.ps1
+++ lab1/OLD/V2-Deploy-Template.ps1
@@ -6,7 +6,7 @@ $template = '.\azuredeploy.json'
 
 az group create -g $rg -l $location
 #Won't work with the message New-AzureRmResourceGroupDeployment : 21:28:31 - Error: Code=InvalidTemplate; Message=Deployment template validation failed:..
-New-AzureRmRe
+New-AzureRmResourceGroupDeployment -Name $job -storageAccount $storageAccount -accounttype "Premium_RAGRS" -TemplateFile $template -ResourceGroupName $rg
 #Will work
 New-AzureRmResourceGroupDeployment -Name $job -storageAccount $storageAccount -TemplateFile $template -ResourceGroupName $rg
 
